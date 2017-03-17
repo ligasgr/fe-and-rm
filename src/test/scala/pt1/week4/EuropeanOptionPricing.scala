1be50dc5a33cb6edad4efc17208e0ab36adf6a56
ligasgr <ligasgr@gmail.com>
2017-03-17 06:06:24 +0000

    European option pricing - done.

--- src/test/scala/pt1/week4/EuropeanOptionPricing.scala
+++ src/test/scala/pt1/week4/EuropeanOptionPricing.scala
@@ -7,27 +7,26 @@ import scala.math.{exp, sqrt}
 object EuropeanOptionPricing {
 
   def calculate(sharePriceLattice: DenseMatrix[Double], termInYears: Double, volatility: Double,
-                numberOfPeriods: Int, interestRate: Double, dividendYield: Double, strikePrice: Double): Double = {
+                numberOfPeriods: Int, interestRate: Double, dividendYield: Double, strikePrice: Double, isPut: Boolean): Double = {
     val u: Double = exp(volatility * sqrt(termInYears / numberOfPeriods))
     val d: Double = 1 / u
     val n = numberOfPeriods + 1
-    val q: Double = exp((interestRate - dividendYield) * termInYears / numberOfPeriods) / (u - d)
+    val q: Double = (exp((interestRate - dividendYield) * termInYears / numberOfPeriods) - d) / (u - d)
     val p: Double = 1 - q
-    val isPut = true
+    val periodRelatedDivisor: Double = exp(interestRate * termInYears / numberOfPeriods)
     val putCallMultiplier: Double = if (isPut) -1 else 1
 
     val result = DenseMatrix.zeros[Double](n, n)
     val lastColumnIndex = numberOfPeriods
-
     val newLastRow = (sharePriceLattice(::, lastColumnIndex) - strikePrice) * putCallMultiplier
     val finalLastRow = max(result(::, lastColumnIndex), newLastRow)
     result(::, lastColumnIndex) := finalLastRow
 
-    for (i <- lastColumnIndex - 1 to 0) {
+    val columnsFromPriorToLastOneDownToFirst = (lastColumnIndex - 1) to 0 by -1
+    for (i <- columnsFromPriorToLastOneDownToFirst) {
       val previousColumn = result(::, i + 1)
-      val oneUp = previousColumn :* u
-      val oneDown = DenseVector.fill(1, previousColumn.valueAt(0) * d)
-      val currentColumn = DenseVector.vertcat(oneDown, oneUp(0 until oneUp.length - 1))
+      val previousColumnShiftedOneDown = DenseVector.vertcat(previousColumn(1 until previousColumn.length), DenseVector.zeros[Double](1))
+      val currentColumn = ((previousColumnShiftedOneDown * q) + (previousColumn * p)) / periodRelatedDivisor
       result(::, i) += currentColumn
     }
 
--- src/test/scala/pt1/week4/EuropeanOptionPricingSpec.scala
+++ src/test/scala/pt1/week4/EuropeanOptionPricingSpec.scala
@@ -4,13 +4,20 @@ import org.scalatest.{FunSuite, Matchers}
 
 class EuropeanOptionPricingSpec extends FunSuite with Matchers {
 
-  test("Price should be as expected for put option") {
-    val sharePriceLattice = SharePriceLattice.generate(initialPrice = 100.0, termInYears = 0.25, volatility = 0.3,
-      numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01)
+  private val sharePriceLattice = SharePriceLattice.generate(initialPrice = 100.0, termInYears = 0.25, volatility = 0.3,
+    numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01)
 
+  test("Price should be as expected for put option") {
     val putOptionPrice = EuropeanOptionPricing.calculate(sharePriceLattice, termInYears = 0.25, volatility = 0.3,
-      numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01, strikePrice = 110.0)
+      numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01, strikePrice = 110.0, isPut = true)
 
     putOptionPrice shouldBe (12.31 +- 0.01)
+  }
+
+  test("Price should be as expected for call option") {
+    val callOptionPrice = EuropeanOptionPricing.calculate(sharePriceLattice, termInYears = 0.25, volatility = 0.3,
+      numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01, strikePrice = 110.0, isPut = false)
+
+    callOptionPrice shouldBe (2.60 +- 0.01)
   }
 }
