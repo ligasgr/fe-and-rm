c53c5997a018614ff1f6a17d42b8d4edeaf9fa1b
ligasgr <ligasgr@gmail.com>
2017-05-08 14:42:49 +0100

    Futures pricing for bonds.

--- src/main/scala/pt1/week4/FuturesPricing.scala
+++ src/main/scala/pt1/week4/FuturesPricing.scala
@@ -5,31 +5,41 @@ import breeze.linalg.{DenseMatrix, DenseVector}
 import scala.math.{exp, sqrt}
 
 object FuturesPricing {
-  def calculate(sharePriceLattice: DenseMatrix[Double], termInYears: Double, volatility: Double,
-                numberOfPeriods: Int, interestRate: Double, dividendYield: Double): Double = {
+  def calculateForShares(sharePriceLattice: DenseMatrix[Double], termInYears: Double, volatility: Double,
+                         numberOfPeriods: Int, interestRate: Double, dividendYield: Double): Double = {
     val result = calculatePricingMatrix(sharePriceLattice, termInYears, volatility, numberOfPeriods, interestRate, dividendYield)
 
     result.apply(0, 0)
   }
 
-  private[week4] def calculatePricingMatrix(sharePriceLattice: DenseMatrix[Double], termInYears: Double, volatility: Double, numberOfPeriods: Int, interestRate: Double, dividendYield: Double) = {
-        val u: Double = exp(volatility * sqrt(termInYears / numberOfPeriods))
-        val d: Double = 1 / u
-        val n = numberOfPeriods + 1
-        val q: Double = (exp((interestRate - dividendYield) * termInYears / numberOfPeriods) - d) / (u - d)
-        val p: Double = 1 - q
+  def calculate(priceLattice: DenseMatrix[Double], numberOfPeriods: Int, q: Double, p: Double): Double = {
+    val result = calculatePricingMatrix(priceLattice, numberOfPeriods, q, p)
 
-        val result = DenseMatrix.zeros[Double](n, n)
-        val lastColumnIndex = numberOfPeriods
-        result(::, lastColumnIndex) := sharePriceLattice(::, lastColumnIndex)
+    result.apply(0, 0)
+  }
 
-        val columnsFromPriorToLastOneDownToFirst = (lastColumnIndex - 1) to 0 by -1
-        for (i <- columnsFromPriorToLastOneDownToFirst) {
-          val previousColumn = result(::, i + 1)
-          val previousColumnShiftedOneDown = DenseVector.vertcat(previousColumn(1 until previousColumn.length), DenseVector.zeros[Double](1))
-          val currentColumn = (previousColumnShiftedOneDown * q) + (previousColumn * p)
-          result(::, i) += currentColumn
-        }
-        result
-      }
+  private[week4] def calculatePricingMatrix(sharePriceLattice: DenseMatrix[Double], termInYears: Double, volatility: Double, numberOfPeriods: Int, interestRate: Double, dividendYield: Double): DenseMatrix[Double] = {
+    val u: Double = exp(volatility * sqrt(termInYears / numberOfPeriods))
+    val d: Double = 1 / u
+    val q: Double = (exp((interestRate - dividendYield) * termInYears / numberOfPeriods) - d) / (u - d)
+    val p: Double = 1 - q
+    calculatePricingMatrix(sharePriceLattice, numberOfPeriods, q, p)
+  }
+
+  private def calculatePricingMatrix(priceLattice: DenseMatrix[Double], numberOfPeriods: Int, q: Double, p: Double): DenseMatrix[Double] = {
+    val n = numberOfPeriods + 1
+
+    val result = DenseMatrix.zeros[Double](n, n)
+    val lastColumnIndex = numberOfPeriods
+    result(::, lastColumnIndex) := priceLattice(::, lastColumnIndex).slice(0, n)
+
+    val columnsFromPriorToLastOneDownToFirst = (lastColumnIndex - 1) to 0 by -1
+    for (i <- columnsFromPriorToLastOneDownToFirst) {
+      val previousColumn = result(::, i + 1)
+      val previousColumnShiftedOneDown = DenseVector.vertcat(previousColumn(1 until previousColumn.length), DenseVector.zeros[Double](1))
+      val currentColumn = (previousColumnShiftedOneDown * q) + (previousColumn * p)
+      result(::, i) += currentColumn
+    }
+    result
+  }
 }
--- src/test/scala/pt1/week4/FuturesPricingSpec.scala
+++ src/test/scala/pt1/week4/FuturesPricingSpec.scala
@@ -1,6 +1,7 @@
 package pt1.week4
 
 import org.scalatest.{FunSuite, Matchers}
+import pt1.week5.{BondPricing, ShortRateLattice}
 
 class FuturesPricingSpec extends FunSuite with Matchers {
 
@@ -8,9 +9,28 @@ class FuturesPricingSpec extends FunSuite with Matchers {
     numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01)
 
   test("Price should be as expected for futures") {
-    val futuresPrice = FuturesPricing.calculate(sharePriceLattice, termInYears = 0.25, volatility = 0.3,
+    val futuresPrice = FuturesPricing.calculateForShares(sharePriceLattice, termInYears = 0.25, volatility = 0.3,
       numberOfPeriods = 15, interestRate = 0.02, dividendYield = 0.01)
 
     futuresPrice shouldBe (100.25 +- 0.01)
+  }
+
+  test("Price should be as expected for futures on bond") {
+    val faceValue = 100.0d
+    val coupon = 0.1d
+    val shortRateLattice = ShortRateLattice.generate(0.06d, 1.25d, 0.9d, 6)
+    val bondPricingMatrix = BondPricing.calculatePricingMatrix(shortRateLattice, faceValue, coupon, maturity = 6, q = 0.5d, p = 0.5d)
+    val noCouponPricingMatrix = bondPricingMatrix - (faceValue * coupon)
+    val futuresPrice = FuturesPricing.calculate(noCouponPricingMatrix, 4, 0.5d, 0.5d)
+
+    futuresPrice shouldBe (103.22 +- 0.01)
+  }
+
+  test("Price should be as expected for futures on zero-coupon bond") {
+    val shortRateLattice = ShortRateLattice.generate(0.05d, 1.1d, 0.9d, 10)
+    val bondPricingMatrix = BondPricing.calculatePricingMatrix(shortRateLattice, faceValue = 100.0d, coupon = 0.0d, maturity = 10, q = 0.5d, p = 0.5d)
+    val futuresPrice = FuturesPricing.calculate(bondPricingMatrix, 4, 0.5d, 0.5d)
+
+    futuresPrice shouldBe (74.82 +- 0.01)
   }
 }
