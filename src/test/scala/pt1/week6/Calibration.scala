d4e1b9c37a09e0e0191bafa29c3c70c581b09fe1
ligasgr <ligasgr@gmail.com>
2017-05-28 01:17:56 +0100

    Swaption pricing using calibrated BDT.

--- src/test/scala/pt1/week6/Calibration.scala
+++ src/test/scala/pt1/week6/Calibration.scala
@@ -8,7 +8,7 @@ object Calibration {
 
   def calibrate(initialParams: DenseVector[Double], valuesToCompareTo: DenseVector[Double])
                (valueComputation: DenseVector[Double] => DenseVector[Double]): DenseVector[Double] = {
-    val lbfgs = new LBFGS[DenseVector[Double]](m = 7, tolerance = 1E-10)
+    val lbfgs = new LBFGS[DenseVector[Double]](m = 7, tolerance = 1E-11)
     val calculation: DenseVector[Double] => Double = { x: DenseVector[Double] =>
       val computedValue = valueComputation(x)
       sum(pow(computedValue - valuesToCompareTo, 2.0d))
--- src/test/scala/pt1/week6/CalibrationSpec.scala
+++ src/test/scala/pt1/week6/CalibrationSpec.scala
@@ -3,14 +3,14 @@ package pt1.week6
 import breeze.linalg.DenseVector
 import common.MatrixMatchers
 import org.scalatest.FunSuite
-import pt1.week5.ForwardEquations
+import pt1.week5.{ForwardEquations, SwapPricing}
 
 class CalibrationSpec extends FunSuite with MatrixMatchers {
 
   test("calibrated parameters should give expected rates") {
-    val initialA = DenseVector.fill[Double](10, 0.03d)
+    val initialA = DenseVector.fill[Double](10, 3.00d)
     val b = 0.05d
-    val observedMarketRates = DenseVector(0.03d, 0.031d, 0.032d, 0.033d, 0.034d, 0.035d, 0.0355d, 0.0360d, 0.0365d, 0.037d)
+    val observedMarketRates = DenseVector(3.00d, 3.1d, 3.2d, 3.3d, 3.4d, 3.5d, 3.55d, 3.60d, 3.65d, 3.7d)
 
     val optimizedA = Calibration.calibrate(initialA, observedMarketRates)(bdtForwardEquations(b))
 
@@ -19,8 +19,36 @@ class CalibrationSpec extends FunSuite with MatrixMatchers {
 
   private def bdtForwardEquations(b: Double) = { a: DenseVector[Double] =>
     val bdt = BDT.calculateRateLattice(a, b, q = 0.5d, p = 0.5d)
-    val (_, spotRates) = ForwardEquations.calculate(1.0d, bdt, q = 0.5d, p = 0.5d)
-    spotRates
+    val (_, spotRates) = ForwardEquations.calculate(1.0d, bdt / 100.0d, q = 0.5d, p = 0.5d)
+    spotRates * 100.00d
+  }
+
+  test("swaption price for calibrated model should be as expected") {
+    val initialA = DenseVector.fill[Double](10, 3.00d)
+    val b = 0.05d
+    val observedMarketRates = DenseVector(3.00d, 3.1d, 3.2d, 3.3d, 3.4d, 3.5d, 3.55d, 3.60d, 3.65d, 3.7d)
+    val optimizedA = Calibration.calibrate(initialA, observedMarketRates)(bdtForwardEquations(b))
+
+    val notional = 1000000.0d
+    val fixedRate = 0.039d
+    val shortRateLattice = BDT.calculateRateLattice(optimizedA, b, q = 0.5d, p = 0.5d)
+    val price = SwapPricing.calculateOption(shortRateLattice / 100.00d, notional, fixedRate, terms = 10, expiration = 3, q = 0.5d, p = 0.5d)
+
+    price shouldBe 4102.00d +- 1.00
+  }
+
+  test("swaption price for calibrated model with different b should be as expected") {
+    val initialA = DenseVector.fill[Double](10, 3.00d)
+    val b = 0.1d
+    val observedMarketRates = DenseVector(3.00d, 3.1d, 3.2d, 3.3d, 3.4d, 3.5d, 3.55d, 3.60d, 3.65d, 3.7d)
+    val optimizedA = Calibration.calibrate(initialA, observedMarketRates)(bdtForwardEquations(b))
+
+    val notional = 1000000.0d
+    val fixedRate = 0.039d
+    val shortRateLattice = BDT.calculateRateLattice(optimizedA, b, q = 0.5d, p = 0.5d)
+    val price = SwapPricing.calculateOption(shortRateLattice / 100.00d, notional, fixedRate, terms = 10, expiration = 3, q = 0.5d, p = 0.5d)
+
+    price shouldBe 8096.00d +- 1.00
   }
 
 }
